c2070f78005e88cb9246c5130f38f21c8001012a
Ellisande <justin.dragos@gmail.com>
2022-08-31 14:29:45 -0700

    Stylesheet unloading example

--- app/routes/coin/index.tsx
+++ app/routes/coin/index.tsx
@@ -1,3 +1,8 @@
+import { LinksFunction } from "@remix-run/node";
+import coinIndexStyleUrl from "../../styles/coinIndex.css";
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: coinIndexStyleUrl }];
+};
 export default function Empty() {
-  return <div>Placeholder</div>;
+  return <div className="coin-text">Choose a coin</div>;
 }
