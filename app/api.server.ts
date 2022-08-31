a0c4f343bb6939a3c77ea2023ce1f15fbeddd6e6
Ellisande <justin.dragos@gmail.com>
2022-08-31 14:27:19 -0700

    Grab api key from .env

--- app/api.server.ts
+++ app/api.server.ts
@@ -1,3 +1,5 @@
-export const ApiKey = "";
+export const getApiKey = () => {
+  return process.env.API_KEY as string;
+};
 export const baseUrl =
   "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency";
