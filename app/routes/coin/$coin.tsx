bf04bc9e7a024bafca4cb7c9c6a2344ba33a0081
Ellisande <justin.dragos@gmail.com>
2022-09-07 15:48:25 -0700

    Remove content to match step 8

--- app/routes/coin/$coin.tsx
+++ app/routes/coin/$coin.tsx
@@ -17,11 +17,8 @@ export const action: ActionFunction = async ({ request }) => {
   const price = Number(body.price);
   const name = body.name;
   const unitsPurchased = 100 / price;
-  // --- Talk to my API or Database, or whatever! --- //
-  console.log(`Bought ${unitsPurchased} units of ${name}`);
-  if (body.name == "Bitcoin") {
-    return json({ error: "Never, ever, buy bitcoin" });
-  }
+  // Step 8 - if the coin name is Bitcoin return an error message
+  // by returning a json object with an error field using the json() function
   const url = new URL(request.url);
   url.searchParams.set("unitsPurchased", String(unitsPurchased));
   return redirect(url.toString());
@@ -46,12 +43,15 @@ export const loader: LoaderFunction = async ({ params, request }) => {
 
 export default function CoinDetails() {
   const { currency, unitsPurchased } = useLoaderData();
-  const { error } = useActionData() || {};
+  // Step 8 - determine if there is any validation errors by using useActionData to fetch
+  // any information returned from the action function
   const { quote } = currency;
   const price = Number(quote.USD.price);
   return (
     <div className="coin-text">
-      {error && <h2>{error}</h2>}
+      {/*
+        Step 8 - if an error is present show it on the screen up here at the top
+      */}
       {unitsPurchased && (
         <h2>You bought {Number(unitsPurchased).toFixed(2)} units</h2>
       )}
