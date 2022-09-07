0777f843ce6ce01cc8b559d84f4836421ab6fc6f
Ellisande <justin.dragos@gmail.com>
2022-09-07 14:28:11 -0700

    Remove content to match step 1

--- app/routes/coin/index.tsx
+++
@@ -1,8 +0,0 @@
-import { LinksFunction } from "@remix-run/node";
-import coinIndexStyleUrl from "../../styles/coinIndex.css";
-export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: coinIndexStyleUrl }];
-};
-export default function Empty() {
-  return <div className="coin-text">Choose a coin</div>;
-}
